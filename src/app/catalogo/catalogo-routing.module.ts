4500f6f7d1ea3a1b7cf9b0f0fe42d2bedbe712ad
alce65 <alce65@hotmail.es>
2019-07-26 13:02:50 +0200

    Guardias

--- src/app/catalogo/catalogo-routing.module.ts
+++ src/app/catalogo/catalogo-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CatalogoComponent } from './catalogo.component';
 import { DetalleComponent } from './detalle/detalle.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 
 const routes: Routes = [
   { path: '', component: CatalogoComponent },
-  { path: 'detalle/:id', component: DetalleComponent },
+  { path: 'detalle/:id', 
+    component: DetalleComponent,
+    canActivate: [AuthGuard]
+   },
   
 ];
 
